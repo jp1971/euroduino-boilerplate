c7901b95c1f8ac20e61107926c0b6da8372b4706
Jameson Proctor <jameson@athleticsnyc.com>
2015-03-31 23:43:16 -0400

    Adding boilerplate file.

---
+++ euroduino-boilerplate.ino
@@ -0,0 +1,51 @@
+ /*
+    Telekinetic     
+                   n
+        |\   |  or
+       _| \-/ic
+      /    un
+    //    ~ + \
+   //         |
+  //    \      \
+ |||     | .  .|
+///     / \___/
+
+*/
+
+/* Euroduino Boilerplate
+ * ---------------------
+ */
+ 
+/*  Copyright 2015 JP1971 (jameson@jp1971.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+// These constants won't change.  They're used to give names
+// to the pins used:
+const int analogIn1Pin = A0;	// Analog Input 1
+const int analogIn2Pin = A1;	// Analog Input 2
+const int analogPot1Pin = A2;	// Pot 1
+const int analogPot2Pin = A3;	// Pot 2
+const int analogOut1Pin = 5;	// Analog Output 1
+const int analogOut2Pin = 6;	// Analog Output 2
+const int DigitalIn1Pin = 8;	// Digital Input 1
+const int DigitalIn2Pin = 9;	// Digital Input 2
+const int DigitalOut1Pin = 3;	// Digital Output 1
+const int DigitalOut2Pin = 4;	// Digital Output 2
+const int Switch1Up = A4;		// Switch 1 Up
+const int Switch1Dwn = A5;		// Switch 1 Dwn
+const int Switch2Up = 7;		// Switch 2 Up
+const int Switch2Dwn = 2;		// Switch 2 Dwn
