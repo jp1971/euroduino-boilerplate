4bf2e307e00974ee02f78be6d38ae166e6118a6c
Jameson Proctor <jameson@athleticsnyc.com>
2015-07-02 22:37:51 -0400

    Working on comment spacing.

--- euroduino-boilerplate.ino
+++ euroduino-boilerplate.ino
@@ -35,7 +35,7 @@
 
 // These constants won't change.  They're used to give names
 // to the pins used:
-const int analogIn1Pin = A0;	// Analog Input 1
+const int analogIn1Pin = A0;    // Analog Input 1
 const int analogIn2Pin = A1;	// Analog Input 2
 const int analogPot1Pin = A2;	// Pot 1
 const int analogPot2Pin = A3;	// Pot 2
@@ -44,8 +44,8 @@ const int analogOut2Pin = 6;	// Analog Output 2
 const int DigitalIn1Pin = 8;	// Digital Input 1
 const int DigitalIn2Pin = 9;	// Digital Input 2
 const int DigitalOut1Pin = 3;	// Digital Output 1
-const int DigitalOut2Pin = 4;	// Digital Output 2
-const int Switch1Up = A4;		// Switch 1 Up
+const int DigitalOut2Pin = 4;    // Digital Output 2
+const int Switch1Up = A4;        // Switch 1 Up
 const int Switch1Dwn = A5;		// Switch 1 Dwn
 const int Switch2Up = 7;		// Switch 2 Up
 const int Switch2Dwn = 2;		// Switch 2 Dwn
